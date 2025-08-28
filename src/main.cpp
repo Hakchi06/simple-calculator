35e5c214f161f330371b2e41ba10a2244aed3ef9
HK <68671793+Hakchi06@users.noreply.github.com>
2025-08-27 22:36:54 -0500

    Update main file
    - Updated the main file to run the application with the model and view.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
-// #include "model/CalculatorModel.h"
-// #include <iostream>
 #include <FL/Fl.H>
 #include "view/CalculatorView.h"
+#include "model/CalculatorModel.h"
+#include "controller/CalculatorController.h"
 
 int main(){
-    CalculatorView view(300, 400, "Calculadora MVC - FLTK");
+    CalculatorView view(300, 400, "Simple Calculator");
+    CalculatorModel model;
+    CalculatorController controller(&model, &view);
+    
     view.show();
     
     return Fl::run();
