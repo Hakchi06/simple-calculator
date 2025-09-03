7b4b2433a6c2ec3c5517b4496de5aab686099e76
HK <68671793+Hakchi06@users.noreply.github.com>
2025-09-03 18:06:40 -0500

    Update Controller implementation
    - add parentheses button functionality and logic
    - add delete button functionality

--- src/controller/CalculatorController.cpp
+++ src/controller/CalculatorController.cpp
@@ -12,12 +12,43 @@ CalculatorController::~CalculatorController() = default;
 void CalculatorController::getButtonValue(const std::string& value){
     if(value == "AC"){
         model->clear();
-        view->setDisplayText("");   
+        view->setDisplayText("");
+        parenthesesClose = true;
     } else if (value == "="){
-        double result = model->calculate();
-        view->setDisplayText(std::to_string(result));
-    } else{
+        if(parenthesesClose){
+            double result = model->calculate();
+            
+            view->setDisplayText(std::to_string(result));
+        }
+    } else if(value == "del"){
+        std::string del = model->getExpression();
+        del.pop_back();
+        model->setExpression(del);
+        view->setDisplayText(model->getExpression());
+    }else if(value == "()"){
+         std::string expr = model->getExpression();
+
+        int openCount = std::count(expr.begin(), expr.end(), '(');
+        int closeCount = std::count(expr.begin(), expr.end(), ')');
+
+        if (openCount == closeCount || expr.empty() || expr.back() == '(' || expr.back() == '+' || expr.back() == '-' || expr.back() == '*' || expr.back() == '/') {
+            //handle when opencount and closecount are 0 or when last char is a number
+            if (expr.back() == ')' || (!expr.empty() && expr.back() != '(')){
+               expr += "*"; // implicit multiplication
+            }
+            model->setExpression(expr + "(");
+            view->setDisplayText(model->getExpression());
+            
+            parenthesesClose = false;
+        } else {
+            model->setExpression(expr + ")");
+            view->setDisplayText(model->getExpression());
+
+            parenthesesClose = true;
+        }
+    } else {
         model->setExpression(model->getExpression() + value);
         view->setDisplayText(model->getExpression());
     }
+    
 }
